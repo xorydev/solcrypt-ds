363a39c26e3990df86dd047165616ac6471ca801
Xory <xorycode@shuriken.pm>
2024-06-10 11:02:50 +0300

    cleanup: move test files to their own directory

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use crypto::{encrypt_file, decrypt_file};
 
 fn main() {
     println!("Hello, world!");
-    encrypt_file("meow.txt", "meow.enc").unwrap();
-    decrypt_file("meow.enc", "meow.dec.text").unwrap();
+    encrypt_file("testfiles/meow.txt", "testfiles/meow.enc").unwrap();
+    decrypt_file("testfiles/meow.enc", "testfiles/meow.dec.text").unwrap();
 }

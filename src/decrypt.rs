9fb51b731e613e577187b2e97b3367ea43052c32
Xory <xorycode@shuriken.pm>
2024-06-17 13:51:32 +0300

    fix: decryptor

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -1,9 +1,9 @@
 mod crypto;
 use crypto::decrypt_directory;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    println!("Encrypting test directory");
-    decrypt_directory("testdir").unwrap();
+    let home_dir = env::var("USERPROFILE").unwrap_or("C:\\Users\\Xory".to_string());
+    decrypt_directory(&home_dir).unwrap();
     
 }

